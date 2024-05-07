8f810bf38e3c05686fd05a4cb20a01aadfdab250
Abhay Sharma <abhays948@gmail.com>
2024-05-07 15:44:25 +0530

    Adding Factory DP

--- Creational/FactoryDP/FactoryDesignPatternMain.java
+++ Creational/FactoryDP/FactoryDesignPatternMain.java
@@ -2,16 +2,18 @@ package DesignPatterns.Creational.FactoryDP;
 
 public class FactoryDesignPatternMain {
     public static void main(String Args[]) {
+
         //Call the factory class
         UsedCarFactory usedCarFactory = new UsedCarFactory();
+
         //Now get the required classes from the factory object created
         CarCompanies company1 = usedCarFactory.getCompanyName("Toyota");
         CarCompanies company2 = usedCarFactory.getCompanyName("Suzuki");
         CarCompanies company3 = usedCarFactory.getCompanyName("Mahindra");
 
-        company1.getName();
-        company2.getName();
-        company3.getName();
+        company1.getName(); //Prints Toyota Class
+        company2.getName(); //Prints Suzuki Class
+        company3.getName(); //Prints Mahindra Class
 
     }
 }
